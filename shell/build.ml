fb600f927768a4ce08a3606abbe4a3d315d4c1d7
Anil Madhavapeddy <anil@recoil.org>
2018-07-03 22:13:38 +0100

    fix result build
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- shell/build.ml
+++ shell/build.ml
@@ -2,10 +2,12 @@ let _ =
   Sys.command (Format.sprintf "cp vendor/result/`ocaml vendor/result/which_result.ml %s` vendor/result/result.ml" Sys.ocaml_version)
 
 let build_bytecode () =
-  print_endline "ocamlc -I vendor/cmdliner/src -I vendor/result unix.cma -o opam-depext `ocamldep -sort vendor/cmdliner/src/*.ml*` vendor/result/result.ml depext.ml"
+  print_endline "ocamlc -c vendor/result/result.ml";
+  print_endline "ocamlc -I vendor/cmdliner/src -I vendor/result unix.cma -o opam-depext `ocamldep -sort vendor/cmdliner/src/*.ml*` depext.ml"
 
 let build_native () =
-  print_endline "ocamlopt -I vendor/cmdliner/src -I vendor/result unix.cmxa -o opam-depext `ocamldep -sort vendor/cmdliner/src/*.ml*` vendor/result/result.ml depext.ml"
+  print_endline "ocamlopt -c vendor/result/result.ml";
+  print_endline "ocamlopt -I vendor/cmdliner/src -I vendor/result unix.cmxa -o opam-depext `ocamldep -sort vendor/cmdliner/src/*.ml*` depext.ml"
 
 let usage () =
   Printf.eprintf "Usage: ocaml build.ml [byte|native]\n\nDefaults to 'native' build.\n%!";
